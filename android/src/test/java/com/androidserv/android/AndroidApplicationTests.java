d2b4f81c5bf9b65b509b48c58b5a2b862e708540
User <vova102204@gmail.com>
2024-09-14 16:21:43 +0700

    created android server

---
+++ android/src/test/java/com/androidserv/android/AndroidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.androidserv.android;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndroidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
